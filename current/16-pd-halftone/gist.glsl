e22376e561d17cf4f221663396d6e460f59b204d
gugray <gabor@berlin.de>
2023-07-23 19:05:14 +0200

    halftone scene / developing ideas

--- current/16-pd-halftone/gist.glsl
+++ current/16-pd-halftone/gist.glsl
@@ -41,6 +41,8 @@ vec2 map(vec3 p) {
     vec2 res = vec2(1e13, 0.);
     float t = time + 500.;
 
+
+    // Torus coz gotta have one
     // {
     //     vec3 q = p;
     //     q -= vec3(5.25, 0., 0.);
@@ -52,6 +54,7 @@ vec2 map(vec3 p) {
     //     res = opU(res, vec2(d, 1.0));
     // }
 
+    // Rotating moony loonies
     // float rad = 2.;
     // {
     //     vec3 q = p;
@@ -62,8 +65,10 @@ vec2 map(vec3 p) {
     //     ax = t * 0.001;
     //     q = doRotX(q, ax);
     //     float d = sdCutHollowSphere(q, rad, 0.1, 0.3);
+    //     d *= 0.9; // This SDF needs a bit of help
     //     res = opU(res, vec2(d, 10.0 + id.x * 0.7 + id.y * 0.2));
     // }
+    // Loony separator walls
     // {
     //     vec3 q = p;
     //     q -= vec3(rad * 2.5, 0.0, -4.0);
@@ -73,50 +78,53 @@ vec2 map(vec3 p) {
     //     res = opU(res, vec2(d, id.x * 0.9 + id.y * 0.7));
     // }
 
-    {
-        vec3 q = p;
-        float ay = t * 0.0001;
-        float sy = sin(t * 0.005) + 1.0;
-        // q = doRotY(q, ay);
-        // q = doRotY(q, c_index);
-        // q = doRotX(q, t * 0.0001);
-        q -= vec3(15., 5., -t * 0.01);
-        // if (sy < 0.1) q.x += 15.0;
 
-        opMod2(q.yz, vec2(10., 12.));
-        float d = sdBox(q, vec3(5., 0.2, 4.));
-        // res = opU(res, vec2(d, 1.0));
-    }
-    {
-        vec3 q = p;
-        q -= vec3(-9.0, 0., 0.);
-        q = doRotX(q, t * 0.0001);
-        q = doRotY(q, t * 0.0002);
-        q = doRotZ(q, t * 0.0002);
-        float d = sdBox(q, vec3(5.0));
-        q = doRotX(q, 1.0);
-        q = doRotY(q, 2.0);
-        q = doRotZ(q, 3.0);
-        d = min(d, sdBox(q, vec3(5.0)));
-        q = doRotX(q, 3.0);
-        q = doRotY(q, 1.0);
-        q = doRotZ(q, 2.0);
-        d = min(d, sdBox(q, vec3(5.0)));
-        res = opU(res, vec2(d, 1.0));
-    }
+    // Infinite conveyor belt
+    // {
+    //     vec3 q = p;
+    //     float ay = t * 0.0001;
+    //     float sy = sin(t * 0.005) + 1.0;
+    //     // q = doRotY(q, ay);
+    //     // q = doRotY(q, c_index);
+    //     // q = doRotX(q, t * 0.0001);
+    //     q -= vec3(15., 5., -t * 0.01);
+    //     opMod2(q.yz, vec2(10., 12.));
+    //     float d = sdBox(q, vec3(5., 0.2, 4.));
+    //     res = opU(res, vec2(d, 1.0));
+    // }
 
-    {
-        vec3 q = p;
-        q -= vec3(11.0, 0.0, 0.0);
-        q = doRotZ(q, t * 0.001);
-        q = doRotX(q, t * 0.001);
-        // float d = length(q.yz - vec2(1.5*sin(q.x), 0.)) - 1.0;
-        // q = doRotY(q, t * 0.001);
-        float dia = 10.0 * exp(-pow(abs(q.y * 0.4), 1.0) * 0.1);
-        float d = sdSpiral(q, dia, 0.5, 1.0);
-        d *= 0.5;
-        // res = opU(res, vec2(d, 1.0));
-    }
+    // Cubes composite
+    // {
+    //     vec3 q = p;
+    //     q -= vec3(-9.0, 0., 0.);
+    //     q = doRotX(q, t * 0.0001);
+    //     q = doRotY(q, t * 0.0002);
+    //     q = doRotZ(q, t * 0.0002);
+    //     float d = sdBox(q, vec3(5.0));
+    //     q = doRotX(q, 1.0);
+    //     q = doRotY(q, 2.0);
+    //     q = doRotZ(q, 3.0);
+    //     d = min(d, sdBox(q, vec3(5.0)));
+    //     q = doRotX(q, 3.0);
+    //     q = doRotY(q, 1.0);
+    //     q = doRotZ(q, 2.0);
+    //     d = min(d, sdBox(q, vec3(5.0)));
+    //     res = opU(res, vec2(d, 1.0));
+    // }
+
+    // Spiral experiment
+    // {
+    //     vec3 q = p;
+    //     q -= vec3(11.0, 0.0, 0.0);
+    //     q = doRotZ(q, t * 0.001);
+    //     q = doRotX(q, t * 0.001);
+    //     // float d = length(q.yz - vec2(1.5*sin(q.x), 0.)) - 1.0;
+    //     // q = doRotY(q, t * 0.001);
+    //     float dia = 10.0 * exp(-pow(abs(q.y * 0.4), 1.0) * 0.1);
+    //     float d = sdSpiral(q, dia, 0.5, 1.0);
+    //     d *= 0.5;
+    //     res = opU(res, vec2(d, 1.0));
+    // }
 
     return res;
 }
@@ -194,13 +202,14 @@ vec4 renderScene(vec2 trgCoord, vec2 trgRes, sampler2D txScene, vec2 sceneRes) {
     }
     else {
         li = pow(liImg, 0.7) + 0.05;
+        li = 0.0;
         hue = 0.;
         sat = 0.;
     }
 
     vec3 clr = hsb2rgb(hue, sat, 1.0);
 
-    if (pow(length(cellXY / rad), 0.5) < li)
+    if (pow(length(cellXY / rad) - 0.05, 0.8) < li)
     return vec4(clr.rgb, 1.);
 
     return vec4(vec3(0.1), 1.);
